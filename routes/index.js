849915da45f1e9267bca740e97b3bfb321379d32
Carlos Henrique <carlos.c.r.w@gmail.com>
2022-02-27 11:19:03 -0300

    Tela de Login

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,13 @@ var express = require('express');
 const { route } = require('express/lib/application');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-<<<<<<< HEAD
-router.get('/cadastro', usuarioController.cadastro)
-router.get('/login', usuarioController.login)
-router.get('/cardapio', usuarioController.cardapio)
 
-=======
->>>>>>> 28f5ae0e0cb452903ef0b97caf2c54408a5ec5c7
+
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,6 @@ var router = express.Router();
 
 const usuarioController = require('../controllers/usuarioControllers')
 
-// /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });                        -- podemos apagar isso? (Moisés)
 
 router.get('/cadastro', usuarioController.cadastro)
 router.get('/login', usuarioController.login)
