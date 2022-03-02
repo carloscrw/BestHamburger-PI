ed7c8d9b409ce6f068ffe48ad9692daaeb73d49e
Carlos Henrique <carlos.c.r.w@gmail.com>
2022-03-01 22:19:10 -0300

    arquivo user modificado

--- routes/users.js
+++ routes/users.js
@@ -4,17 +4,7 @@ var router = express.Router();
 // const usuarioController = require('../controllers/usuarioControllers')
 
 
-<<<<<<< HEAD
-router.get('/cadastro', usuarioController.cadastro)
-router.get('/login', usuarioController.login)
-router.get('/minhaconta', usuarioController.minhaConta)
-
-=======
 
 
-// router.get('/cadastro', usuarioController.cadastro)
-// router.get('/login', usuarioController.login)
-// router.get('/minhaConta', usuarioController.minhaConta)
->>>>>>> 456d4314236b629c45d54dd1ed768ba5bf3b50f9
 
 module.exports = router;
