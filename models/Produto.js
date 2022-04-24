debb982f5b088cd34ef34ea25e60a09a93b49502
Carlos Henrique <carlos.c.r.w@gmail.com>
2022-04-24 02:04:17 -0300

    feat: model Produto

---
+++ models/Produto.js
@@ -0,0 +1,38 @@
+const Produto = (sequelize, DataTypes) => {
+    const produto = sequelize.define(
+        'Produto', {
+            nome: {
+                type: DataTypes.STRING,
+                allownull: false
+            },
+            preco: {
+                type: DataTypes.DECIMAL,
+                allownull: false
+            },
+            descricao: {
+                type: DataTypes.STRING,
+                allownull: false
+            },
+            quantidade: {
+                type: DataTypes.INTEGER,
+                allownull: false
+            },
+            img: {
+                type: DataTypes.STRING,
+                allownull: false
+            }
+
+
+        }, {
+            tableName: "produto",
+            timestamps: false
+        }
+    )
+    Produto.associate = (models => {
+        Produto.belongsToMany(models.Produto, { as: "pedido", through: "pedido_produto", foreignKey: 'produto_id', otherKey: "pedido_id", timestamps: false })
+    })
+
+    return Produto
+}
+
+module.exports = Produto
