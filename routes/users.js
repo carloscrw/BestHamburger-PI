d0fc6af3e65919c5960e5e2ef31020c50e7557e1
verox13 <veronica.bandeira1990@yahoo.com.br>
2022-05-01 19:44:48 -0300

    feat:  route post em serviços

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,13 @@ var router = express.Router();
 const usuarioController = require('../controllers/usuarioControllers')
 
 router.get('/cadastro', usuarioController.cadastro)
+router.post('/cadastro', usuarioController.cadastro)
 router.get('/login', usuarioController.login)
+router.post('/login', usuarioController.login)
 router.get('/minhaConta', usuarioController.minhaConta)
+router.post('/minhaConta', usuarioController.minhaConta)
 router.get('/carrinho', usuarioController.carrinho)
+router.post('/carrinho', usuarioController.carrinho)
 
 
 
