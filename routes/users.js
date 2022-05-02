9ccb7c1d2fda272c6f280890b7d813847f7ae6fe
verox13 <veronica.bandeira1990@yahoo.com.br>
2022-05-01 23:14:40 -0300

    feat: pasta middlewarwares

--- routes/users.js
+++ routes/users.js
@@ -8,9 +8,8 @@ router.post('/cadastro', usuarioController.cadastro)
 router.get('/login', usuarioController.login)
 router.post('/login', usuarioController.login)
 router.get('/minhaConta', usuarioController.minhaConta)
-router.post('/minhaConta', usuarioController.minhaConta)
 router.get('/carrinho', usuarioController.carrinho)
-router.post('/carrinho', usuarioController.carrinho)
+
 
 
 
