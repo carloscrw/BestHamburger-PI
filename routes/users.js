471ae17ec6045fd9493428b8e787e2882620f17f
Carlos Henrique <carlos.c.r.w@gmail.com>
2022-05-03 00:18:01 -0300

    Cadastro de Usuario

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const usuarioController = require('../controllers/usuarioControllers')
 
 router.get('/cadastro', usuarioController.cadastro)
-router.post('/cadastro', usuarioController.cadastro)
+router.post('/cadastro', usuarioController.cadastroUsuario)
 router.get('/login', usuarioController.login)
 router.post('/login', usuarioController.login)
 router.get('/minhaConta', usuarioController.minhaConta)
